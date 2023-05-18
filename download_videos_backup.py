75ae5019fc5c3bff5a8d36120c6a488dda9ca4ab
zeusofyork <storeandbills@gmail.com>
2023-05-18 12:50:40 -0400

    changed downloader to download each video into it's own folder and also creates a txt file which includes the posts's title and URL via its post.id

--- download_videos_backup.py
+++ download_videos_backup.py
@@ -1,5 +1,6 @@
 import praw
 import requests
+import os
 
 # Initialize the Reddit API client
 reddit = praw.Reddit(
@@ -20,16 +21,48 @@ for post in posts:
     # Check if the post has a video
     if post.is_video:
         video_url = post.media['reddit_video']['fallback_url']
+        title = str('post_title: ' + post.title)
+        #post_id = str('post_id: ' + post.id)
+        post_url = str('post_url: ' + 'reddit.it/' + post.id)
+        post_info = [title, post_url]
         # Remove the extension from the URL to get the filename
         filename = str(post.id)
         print(f"Downloading video: {filename}")
+
+        # Create a folder for each video using the filename
+        folder_name = os.path.join('downloads', filename)
+        os.makedirs(folder_name, exist_ok=True)
+        with open((os.getcwd() + '/downloads/' + filename + '/' + filename +'.txt') , 'w') as post_details:
+            for item in post_info:
+                post_details.write(item)
+                post_details.write('\n')
         
         # Send a request to download the video
         response = requests.get(video_url)
+
         
+        # Save the video to a file inside the folder
+        file_path = os.path.join(folder_name, f"{filename}.mp4")
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+            
+
+            '''
+            for item in post_content:
+                print(item)
+                
+                post_info.write(str(item))
+                post_info.write('\n')
+             '''   
+        
+        print(f"Video downloaded: {filename}")
+
+
+        '''
         # Save the video to a file
         with open(filename, 'wb') as file:
             file.write(response.content)
         
         print(f"Video downloaded: {filename}")
         
+        '''
