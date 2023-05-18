873a13c5396c5b6de31616c0f909e9b0be9c9801
zeusofyork <storeandbills@gmail.com>
2023-05-18 13:26:11 -0400

    added checks to see if video already exists, if it does, allows user to select y to skip over writing or n to overwriting existing folder and files

--- download_videos.py
+++ download_videos.py
@@ -22,30 +22,43 @@ for post in posts:
     if post.is_video:
         video_url = post.media['reddit_video']['fallback_url']
         title = str('post_title: ' + post.title)
-        #post_id = str('post_id: ' + post.id)
         post_url = str('post_url: ' + 'reddit.it/' + post.id)
         post_info = [title, post_url]
         # Remove the extension from the URL to get the filename
         filename = str(post.id)
         print(f"Downloading video: {filename}")
 
-        # Create a folder for each video using the filename
+        # Create a folder for each video using the filename if it doesn't exist
         folder_name = os.path.join('downloads', filename)
+        if os.path.exists(folder_name):
+            choice = input(f"The folder '{folder_name}' already exists. Do you want to skip? (y/n): ")
+            if choice.lower() == 'y':
+                print(f"Skipping folder: {folder_name}")
+                continue
+            else:
+                print(f"Folder: {folder_name}")
+        
         os.makedirs(folder_name, exist_ok=True)
-        with open((os.getcwd() + '/downloads/' + filename + '/' + filename +'.txt') , 'w') as post_details:
+        with open(os.path.join(folder_name, f"{filename}.txt"), 'w') as post_details:
             for item in post_info:
                 post_details.write(item)
                 post_details.write('\n')
         
+        # Check if the video file already exists
+        video_file_path = os.path.join(folder_name, f"{filename}.mp4")
+        if os.path.exists(video_file_path):
+            choice = input(f"The file '{video_file_path}' already exists. Do you want to skip? (y/n): ")
+            if choice.lower() == 'y':
+                print(f"Skipping video: {video_file_path}")
+                continue
+            else:
+                print(f"Video: {video_file_path}")
+        
         # Send a request to download the video
         response = requests.get(video_url)
 
-        
         # Save the video to a file inside the folder
-        file_path = os.path.join(folder_name, f"{filename}.mp4")
-        with open(file_path, 'wb') as file:
+        with open(video_file_path, 'wb') as file:
             file.write(response.content)
-            
 
         print(f"Video downloaded: {filename}")
-
